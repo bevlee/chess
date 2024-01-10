fb3e43b9f99211d0552bf4516903ce8f871e121b
bev <bevan.lee@live.com.au>
2024-01-11 01:19:00 +1100

    fix up testing format

--- chess.spec.js
+++ chess.spec.js
@@ -1,13 +1,73 @@
-const expect = import("chai").expect;
+import { expect } from "chai";
 var assert = import("assert");
 
 import { getValidSquares, pawnMoves } from "./chess.js";
 
+const DEFAULT_CHESS_BOARD = [
+  [
+    "white-rook",
+    "white-knight",
+    "white-bishop",
+    "white-queen",
+    "white-king",
+    "white-bishop",
+    "white-knight",
+    "white-rook",
+  ],
+  [
+    "white-pawn",
+    "white-pawn",
+    "white-pawn",
+    "white-pawn",
+    "white-pawn",
+    "white-pawn",
+    "white-pawn",
+    "white-pawn",
+  ],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [
+    "black-rook",
+    "black-knight",
+    "black-bishop",
+    "black-queen",
+    "black-king",
+    "black-bishop",
+    "black-knight",
+    "black-rook",
+  ],
+  [
+    "black-pawn",
+    "black-pawn",
+    "black-pawn",
+    "black-pawn",
+    "black-pawn",
+    "black-pawn",
+    "black-pawn",
+    "black-pawn",
+  ],
+];
+
 describe("valid pieces moving", () => {
   describe("pawn moving", () => {
-    it("should return corerect moves for an unmoved pawn", () => {
-      const CHESS_BOARD = [...Array(8)].map((x) => [0, 0, 0, 0, 0, 0, 0, 0]);
-      expect(pawnMoves(1, 1, "white", CHESS_BOARD).to.equal(1));
+    describe("should return correct moves for an unmoved white a pawn", () => {
+      const validMoves = pawnMoves(1, 0, "white", DEFAULT_CHESS_BOARD);
+
+      expect(validMoves).to.deep.equal([
+        [1, 2],
+        [1, 3],
+      ]);
+    });
+
+    describe("should return correct moves for an unmoved white f pawn", () => {
+      const validMoves = pawnMoves(0, 7, "white", DEFAULT_CHESS_BOARD);
+      console.log(validMoves);
+      expect(validMoves).to.deep.equal([
+        [8, 2],
+        [8, 3],
+      ]);
     });
   });
 });
